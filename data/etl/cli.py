4e0e85c7aa855293401f32caf4f1bfb9c1393093
jmh <jeff@jmh.dev>
2023-07-01 23:54:39 -0700

    Install requests

---
+++ data/etl/cli.py
@@ -0,0 +1,7 @@
+def cli():
+    """NASA API context
+        'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=';
+        '&format=json'
+        select+*+from+ps+where+pl_rade+<+=+1.8+and+pl_masse+>+0'
+    """
+    return True
