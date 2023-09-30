ecb4ecfb79cdb55183ecc5c3bb75afbfdeba34dd
jmh <jeff@jmh.dev>
2023-09-29 20:38:33 -0700

    Fix vscode settings for tests

--- data/src/query/api.py
+++ data/src/query/api.py
@@ -6,6 +6,14 @@ from query.nasa import NasaQuery
 
 
 def get_planets(nasa_query: NasaQuery):
+    """_summary_
+
+    Args:
+        nasa_query (NasaQuery): _description_
+
+    Returns:
+        _type_: _description_
+    """
     base = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query="
     url = f"{base}{str(nasa_query)}"
     response = requests.get(url, timeout=5000)
--- data/test/test_etl/test_get_planets.py
+++ data/test/test_etl/test_get_planets.py
@@ -1,9 +1,10 @@
+from query.api import get_planets
 from query.nasa import NasaQuery
 
 
-def xtest_get_planets():
+def test_get_planets():
     nasa_query = NasaQuery("*", "ps", "pl_rade+<+=+1.8+and+pl_masse+>+0")
-    result = ""
+    result = get_planets(nasa_query)
 
     assert result is not None
     assert len(result) > 0
